71fd847a435d0ee94710c4970b7808916b60906d
marynavaroli <67209505+marynavaroli@users.noreply.github.com>
2025-02-22 01:29:20 -0500

    Fix large screen responsiveness and add list expand transition

--- Resources/JS/index.js
+++ Resources/JS/index.js
@@ -4,36 +4,49 @@ let edup = document.getElementById('edup');
 function expandUlEdu (event) {
     const element = event.target;
     element.style.transform = 'rotate(45deg)';
+    element.style.display = "none";
         if ( document.documentElement.clientWidth < 625 && element.style.gridRow !== '8 / span 1') {      
             element.style.gridRow = '8 / span 1';
             edup.style.gridRow = '8 / span 3';
-            
             edup.style.height = '446px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 624 && document.documentElement.clientWidth < 826 && element.style.gridRow !== '5 / span 1') {
             element.style.gridRow = '5 / span 1';
             edup.style.gridRow = '5 / span 3';
-
             edup.style.height = '330px';
             element.style.marginTop = '50px';
             edup.style.marginTop = '50px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 824 && document.documentElement.clientWidth < 976 && element.style.gridRow !== '5 / span 1') {
             element.style.gridRow = '5 / span 1';
             edup.style.gridRow = '5 / span 3';
-
             edup.style.height = '302px';
             element.style.marginTop = '52px';
             edup.style.marginTop = '52px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 975 && element.style.gridRow !== '7 / span 1') {
             element.style.gridRow = '7 / span 1';
             edup.style.gridRow = '7 / span 5';
-
             edup.style.height = '371px';
             element.style.marginTop = '30px';
             edup.style.marginTop = '30px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else {
+            element.style.display = "none";
+            setTimeout(()=>{
+                element.style.gridRow = '';
+                edup.style.gridRow = '';
+                element.style.display = "inline-block";
+              }, 500);
             element.style.transform = '';
-            element.style.gridRow = '';
-            edup.style.gridRow = '';
             element.style.height = '';
             edup.style.height = '';
             element.style.marginTop = '';
@@ -67,36 +80,48 @@ let workp = document.getElementById('workp');
 function expandUlWork (event) {
     const element = event.target;
     element.style.transform = 'rotate(45deg)';
+    element.style.display = "none";
         if ( document.documentElement.clientWidth < 625 && element.style.gridRow !== '13 / span 1') {      
             element.style.gridRow = '13 / span 1';
             workp.style.gridRow = '13 / span 3';
-            
             workp.style.height = '446px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 624 && document.documentElement.clientWidth < 826 && element.style.gridRow !== '8 / span 1') {
             element.style.gridRow = '8 / span 1';
             workp.style.gridRow = '8 / span 3';
-            
             workp.style.height = '330px';
             element.style.marginTop = '50px';
             workp.style.marginTop = '50px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 824 && document.documentElement.clientWidth < 976 && element.style.gridRow !== '8 / span 1') {
             element.style.gridRow = '8 / span 1';
             workp.style.gridRow = '8 / span 3';
-            
             workp.style.height = '302px';
             element.style.marginTop = '52px';
             workp.style.marginTop = '52px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 975 && element.style.gridRow !== '12 / span 1') {
             element.style.gridRow = '12 / span 1';
             workp.style.gridRow = '12 / span 5';
-            
             workp.style.height = '371px';
             element.style.marginTop = '30px';
             workp.style.marginTop = '30px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else {
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+                element.style.gridRow = '';
+                workp.style.gridRow = '';
+              }, 500);
             element.style.transform = '';
-            element.style.gridRow = '';
-            workp.style.gridRow = '';
             element.style.height = '';
             workp.style.height = '';
             element.style.marginTop = '';
@@ -131,36 +156,48 @@ let hobbyp = document.getElementById('hobbyp');
 function expandUlHobby (event) {
     const element = event.target;
     element.style.transform = 'rotate(45deg)';
+    element.style.display = "none";
         if ( document.documentElement.clientWidth < 625 && element.style.gridRow !== '18 / span 1') {      
             element.style.gridRow = '18 / span 1';
             hobbyp.style.gridRow = '18 / span 3';
-            
             hobbyp.style.height = '446px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 624 && document.documentElement.clientWidth < 826 && element.style.gridRow !== '11 / span 1') {
             element.style.gridRow = '11 / span 1';
             hobbyp.style.gridRow = '11 / span 3';
-
             hobbyp.style.height = '350px';
             element.style.marginTop = '30px';
             hobbyp.style.marginTop = '30px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 824 && document.documentElement.clientWidth < 976 && element.style.gridRow !== '11 / span 1') {
             element.style.gridRow = '11 / span 1';
             hobbyp.style.gridRow = '11 / span 3';
-
             hobbyp.style.height = '324px';
             element.style.marginTop = '30px';
             hobbyp.style.marginTop = '30px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else if (document.documentElement.clientWidth > 975 && element.style.gridRow !== '17 / span 1') {
             element.style.gridRow = '17 / span 1';
             hobbyp.style.gridRow = '17 / span 5';
-
             hobbyp.style.height = '371px';
             element.style.marginTop = '30px';
             hobbyp.style.marginTop = '30px';
+            setTimeout(()=>{
+                element.style.display = "inline-block";
+              }, 500);
         } else {
+            setTimeout(()=>{
+                element.style.gridRow = '';
+                hobbyp.style.gridRow = '';
+                element.style.display = "inline-block";
+              }, 500);
             element.style.transform = '';
-            element.style.gridRow = '';
-            hobbyp.style.gridRow = '';
             element.style.height = '';
             hobbyp.style.height = '';
             element.style.marginTop = '';
